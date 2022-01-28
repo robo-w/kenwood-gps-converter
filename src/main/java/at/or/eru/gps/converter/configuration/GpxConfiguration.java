2995f0fa112c34774564b8cc09b1ca83b6f465c4
Robert Wittek <r.m.wittek@gmail.com>
2022-01-29 00:18:27 +0100

    update 3rd party dependencies

--- src/main/java/at/or/eru/gps/converter/configuration/GpxConfiguration.java
+++ src/main/java/at/or/eru/gps/converter/configuration/GpxConfiguration.java
@@ -1,7 +1,7 @@
 /*
  * Licensed under MIT license. See LICENSE for details.
  *
- * Copyright (c) 2019 Robert Wittek, https://github.com/robo-w
+ * Copyright (c) 2022 Robert Wittek, https://github.com/robo-w
  */
 
 package at.or.eru.gps.converter.configuration;
@@ -15,7 +15,8 @@ public class GpxConfiguration {
     public GpxConfiguration(
             final int positionLimit,
             final long maximumTrackTimeInMinutes,
-            final String gpxOutputDirectory, final long trackTimeExceededCheckIntervalInMinutes) {
+            final String gpxOutputDirectory,
+            final long trackTimeExceededCheckIntervalInMinutes) {
         this.positionLimit = positionLimit;
         this.maximumTrackTimeInMinutes = maximumTrackTimeInMinutes;
         this.gpxOutputDirectory = gpxOutputDirectory == null ? null : gpxOutputDirectory.trim();
